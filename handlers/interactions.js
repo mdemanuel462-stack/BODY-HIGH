f4ec171e5ec7a8aee8b512e288f7cd94bae6704b
MANUEL <mdemanuel462@gmail.com>
2026-04-17 04:25:14 -0400

    Create interactions.js

---
+++ handlers/interactions.js
@@ -0,0 +1,27 @@
+module.exports = async (interaction) => {
+
+    if (interaction.isButton()) {
+
+        if (interaction.customId === 'btn1') {
+            return interaction.reply({
+                content: '🔥 Botón 1 presionado',
+                ephemeral: true
+            });
+        }
+
+        if (interaction.customId === 'btn2') {
+            return interaction.reply({
+                content: 'ℹ️ Info',
+                ephemeral: true
+            });
+        }
+    }
+
+    if (interaction.isStringSelectMenu()) {
+
+        return interaction.reply({
+            content: `Elegiste: ${interaction.values[0]}`,
+            ephemeral: true
+        });
+    }
+};
