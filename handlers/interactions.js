6abb654ce2e2fa3e4626ccc1e7fee6f30a6ff500
MANUEL <mdemanuel462@gmail.com>
2026-04-19 05:59:51 -0400

    Update interactions.js

--- handlers/interactions.js
+++ handlers/interactions.js
@@ -1,27 +1,31 @@
+const { EmbedBuilder } = require('discord.js');
+
 module.exports = async (interaction) => {
+
+    const reply = (txt) => {
+        return interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setColor('#EC1D1D')
+                    .setDescription(txt)
+            ],
+            ephemeral: true
+        });
+    };
 
     if (interaction.isButton()) {
 
         if (interaction.customId === 'btn1') {
-            return interaction.reply({
-                content: '🔥 Botón 1 presionado',
-                ephemeral: true
-            });
+            return reply('Botón 1 presionado');
         }
 
         if (interaction.customId === 'btn2') {
-            return interaction.reply({
-                content: 'ℹ️ Info',
-                ephemeral: true
-            });
+            return reply('Información mostrada');
         }
     }
 
     if (interaction.isStringSelectMenu()) {
 
-        return interaction.reply({
-            content: `Elegiste: ${interaction.values[0]}`,
-            ephemeral: true
-        });
+        return reply(`Elegiste: **${interaction.values[0]}**`);
     }
 };
