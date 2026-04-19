a7782e6251274d6ef3807218e680185b2e9a9317
MANUEL <mdemanuel462@gmail.com>
2026-04-19 06:51:58 -0400

    Update unticky.js

--- handlers/unticky.js
+++ handlers/unticky.js
@@ -1,1 +1,27 @@
+const { PermissionsBitField, EmbedBuilder } = require('discord.js');
+const stickySystem = require('../handlers/sticky');
 
+module.exports = {
+    name: 'unsticky',
+
+    async execute(message) {
+
+        const sticky = stickySystem.data;
+        const g = message.guild.id;
+        const c = message.channel.id;
+
+        if (!message.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+            return message.channel.send({
+                embeds: [new EmbedBuilder().setColor('#EC1D1D').setDescription('❌ No tienes permisos.')]
+            });
+        }
+
+        if (sticky[g]) delete sticky[g][c];
+
+        stickySystem.save();
+
+        message.channel.send({
+            embeds: [new EmbedBuilder().setColor('#EC1D1D').setDescription('❌ Sticky eliminado')]
+        });
+    }
+};
